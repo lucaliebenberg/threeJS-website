bcd12ef8e0a12bc06e8c9a2bf944f12705de79c1
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-09 17:16:19 +0200

    Color animation added

--- threejs-project/threeJS-website/main.js
+++ threejs-project/threeJS-website/main.js
@@ -73,3 +73,26 @@ const tl = gsap.timeline({ defaults: { durationL: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
 tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+
+// Mouse animation color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    // Let's animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
