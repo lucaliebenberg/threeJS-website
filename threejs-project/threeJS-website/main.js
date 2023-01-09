bbdc8cb434e9fdf7337c97bd081e3e99ea57a791
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-09 16:37:21 +0200

    Styling add to fix white border

--- threejs-project/threeJS-website/main.js
+++ threejs-project/threeJS-website/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import "./style.css";
 
 // Scene
 const scene = new THREE.Scene();
@@ -11,18 +12,29 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 // Light
 const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 10, 10);
+light.position.set(0, 10, 10); // x, y, z positions
 scene.add(light);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 camera.position.z = 20;
 scene.add(camera);
 
 // Renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
